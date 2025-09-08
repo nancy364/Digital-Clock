79dea1a704099d3e52b28b2a1117ae1899ffbb9e
Nancy jha <nancyjha364@gmail.com>
2025-09-08 19:56:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+ let clock = document.querySelector('.container');
+
+ function getCurrentTime(){
+    let date = new Date();
+    return date.toLocaleTimeString();
+ }
+
+ setInterval(()=>{
+ clock.innerHTML = getCurrentTime();
+ },1000);
